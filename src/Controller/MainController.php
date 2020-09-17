be4194d58cbd1237a1a0224b7dd271eb645bd9b8
Roman Maruniak <roman@lanars.com>
2020-09-17 10:46:26 +0400

    fix php version, add information for stat project

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use GuzzleHttp\Client;
 
 class MainController extends AbstractController
 {
@@ -18,5 +19,10 @@ class MainController extends AbstractController
         return $this->render(
             'index.html.twig'
         );
+    }
+    public function getResponse($lang, $word) {
+	    $client = new Client(['base_uri' => 'https://od-api.oxforddictionaries.com/api/v2/entries/']);
+	    $request = $client->request('GET', '/en-us/tea', ['headers' => ['app_id' => '292a9259', 'app_key' => '8f02a9c77948c00d8292034ad5977de0']]);
+        dd($request);
     }
 }
